a95333bed100d9276f6816c3af400e4cd6fd4879
Dahir Warsame <dahirwarsame@gmail.com>
2021-04-11 21:43:57 +0200

    added https://start.spring.io/ bootstrap to repo

---
+++ src/main/java/com/dahirwarsame/iacrest/IacrestApplication.java
@@ -0,0 +1,13 @@
+package com.dahirwarsame.iacrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IacrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IacrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dahirwarsame/iacrest/IacrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dahirwarsame.iacrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IacrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
